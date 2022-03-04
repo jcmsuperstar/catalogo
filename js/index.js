5814eb6a43d0dd27fd116f8f4ed6e05e4fb72941
jcmsuperstar <coreiconid@gmail.com>
2022-03-03 20:04:53 -0600

    modal opciones

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 M.AutoInit();
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-        direction: 'top',
-        hoverEnabled: false
-    });
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
 });
+
+var instance = M.Modal.getInstance(elem);
