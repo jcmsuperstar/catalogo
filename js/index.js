a140ff82ca5034b4ddc2d7b48ee7190c564e5721
jcmsuperstar <coreiconid@gmail.com>
2022-03-04 20:22:05 -0600

    rotate boton

--- js/index.js
+++ js/index.js
@@ -4,5 +4,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 });
-
-var instance = M.Modal.getInstance(elem);
