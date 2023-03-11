a6a0cf1e1bcd4e6c599639103baf06a30cf3932d
jcmsuperstar <68719464+jcmsuperstar@users.noreply.github.com>
2023-03-11 13:13:41 -0600

    a

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'up',
+      hoverEnabled: false
+    });
+  });
