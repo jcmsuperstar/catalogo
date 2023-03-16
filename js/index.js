f5f7028320de0d83a6de5786b2aa9ac50d9c175e
jcmsuperstar <68719464+jcmsuperstar@users.noreply.github.com>
2023-03-15 21:55:36 -0600

    a

--- js/index.js
+++ js/index.js
@@ -5,19 +5,34 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
       direction: 'up',
       hoverEnabled: false
     });
-  });
+  });*/
 
-  const btn = document.getElementById("reporte");
+  const btn = document.getElementById("miBoton");
   const ventana = document.getElementById("report1");
-  
-  btn.onclick = () => {
-    ventana.hidden = !ventana.hidden;
-  }
 
- 
+  
+  /*btn.onclick = () => {
+    ventana.hidden = !ventana.hidden;
+  }*/
+
+  function solicitarContrasena() {
+    const contrasena = prompt("Ingrese la contraseña:");
+  
+    if (contrasena === "A1w2e3r4t5.0") {
+      alert("¡Contraseña correcta!");
+    } else {
+      alert("¡Contraseña incorrecta!");
+    }
+     
+    if (contrasena === "A1w2e3r4t5.0") {
+      ventana.hidden = !ventana.hidden;
+    } else {
+      ""
+    }
+    }
